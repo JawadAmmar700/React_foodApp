639d8c66ec98e0f9032926b4c39f98bcc91f209d
jawadGitNext <jawadGitNext2021@gmail.com>
2021-08-30 11:52:56 +0300

    subscription

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,65 +1,56 @@
-import React, { useState as State } from 'react'
-import Input from '@material-ui/core/Input';
-import Button from '@material-ui/core/Button';
-import '../CSS folder/footer.css'
-
-
+import React, { useState as State } from "react"
+import Input from "@material-ui/core/Input"
+import Button from "@material-ui/core/Button"
+import "../CSS folder/footer.css"
+import axios from "axios"
 
 const footer = () => {
-    const [input, setInput] = State('')
-    const [subscribed, setSubscribed] = State(false)
+  const [input, setInput] = State("")
+  const [subscribed, setSubscribed] = State(false)
 
-    const handleSub = async (e) => {
-        e.preventDefault()
-        if (input) {
-
-            await fetch('http://localhost:5000/', {
-                method: 'POST',
-                mode: 'cors',
-                cache: 'no-cache',
-                credentials: 'same-origin',
-                headers: {
-                    'Content-Type': 'application/json'
-
-                },
-                redirect: 'follow',
-                referrerPolicy: 'no-referrer',
-                body: JSON.stringify({ form_email: input })
-
-            }).then(response => response.json())
-                .then(data => {
-                    console.log('Success:', data);
-                    setSubscribed(true)
-                }).catch(err => console.log(err))
-        }
-
-        setInput('')
+  const handleSub = async e => {
+    e.preventDefault()
+    if (input) {
+      await axios
+        .post("http://localhost:5000", {
+          form_email: input,
+        })
+        .then(data => {
+          setSubscribed(true)
+        })
+        .catch(err => console.log(err))
     }
+    setInput("")
+  }
 
+  return (
+    <div className="footer">
+      <div className="footer_text">
+        Subscribtion is exclusively respectfull for us ❤
+      </div>
 
-
-
-
-
-    return (
-        <div className="footer">
-
-            <div className="footer_text">
-                Subscribtion is exclusively
-                respectfull for us ❤
-            </div>
-
-
-            <form className="subscribe" onSubmit={handleSub}>
-                <p>Subscribe</p>
-                <Input className="input_sub" placeholder="Enter your Email" required value={input} type='text' onChange={e => setInput(e.target.value)} />
-                <Button type='submit' className="btn_sub" variant="contained" color={subscribed ? "primary" : 'secondary'} disabled={subscribed && true}>
-                    {subscribed ? 'Thank you' : "Subscribe"}
-                </Button>
-            </form>
-
-        </div>
-    )
+      <form className="subscribe" onSubmit={handleSub}>
+        <p>Subscribe</p>
+        <Input
+          className="input_sub"
+          placeholder="Enter your Email"
+          required
+          value={input}
+          type="text"
+          onChange={e => setInput(e.target.value)}
+        />
+        <Button
+          type="submit"
+          className="btn_sub"
+          variant="contained"
+          color={subscribed ? "primary" : "secondary"}
+          disabled={subscribed ? true : false}
+        >
+          {subscribed ? "Thank you" : "Subscribe"}
+        </Button>
+      </form>
+    </div>
+  )
 }
 
 export default footer
